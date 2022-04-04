d6adb05f43bebca7393ed336e3296433e2ba0921
Tamera Lanham <tamera.lanham@gmail.com>
2022-04-04 11:32:36 -0700

    refactor: data utils return single-epoch iterators

--- loss_basins/data.py
+++ loss_basins/data.py
@@ -1,31 +1,42 @@
+from typing import Any, Callable, Iterator
 import torch as t
 import torchvision
 
 
-def random_normal(input_shape: tuple[int], output_shape: tuple[int]):
-    while True:
+def random_normal(
+    input_shape: tuple[int], output_shape: tuple[int], n_batches=1000
+) -> Iterator[tuple[t.Tensor, t.Tensor]]:
+    for _ in n_batches:
         inputs = t.randn(input_shape)
         outputs = t.randn(output_shape)
         yield inputs, outputs
 
 
-def identity_normal(shape: tuple[int]):
-    while True:
+def identity_normal(
+    shape: tuple[int], n_batches=1000
+) -> Iterator[tuple[t.Tensor, t.Tensor]]:
+    for _ in n_batches:
         data = t.randn(shape)
         yield data, data
 
 
-def fake_mnist(batch_size):
-    input_shape, input_dtype = (batch_size, 1, 28, 28), t.float32
-    output_shape, ouptut_dtype = (batch_size,), t.int32
+def fake_mnist(batch_size: int, train=True) -> Iterator[tuple[t.Tensor, t.Tensor]]:
 
-    while True:
-        inputs = t.rand(input_shape, dtype=input_dtype)
-        outputs = t.randint(0, 10, output_shape, dtype=ouptut_dtype)
-        yield inputs, outputs
+    input_batch = lambda batch_size: t.rand((batch_size, 1, 28, 28), dtype=t.float32)
+    output_batch = lambda batch_size: t.randint(0, 10, (batch_size,), dtype=t.float32)
+
+    total_samples = 60000 if train else 10000
+    n_standard_batches = total_samples // batch_size
+
+    for _ in range(n_standard_batches):
+        yield input_batch(batch_size), output_batch(batch_size)
+
+    final_batch_size = total_samples % batch_size
+    if final_batch_size:
+        yield input_batch(final_batch_size), output_batch(final_batch_size)
 
 
-def _mnist_loader(batch_size, train=True):
+def _mnist_loader(batch_size: int, train=True) -> Iterator[tuple[t.Tensor, t.Tensor]]:
 
     dataset = torchvision.datasets.MNIST(
         root="./_data",
@@ -37,9 +48,8 @@ def _mnist_loader(batch_size, train=True):
     return loader
 
 
-def mnist(batch_size, train=True):
-    while True:
-        data = _mnist_loader(batch_size, train=True)
+def mnist(batch_size: int, train=True) -> Iterator[tuple[t.Tensor, t.Tensor]]:
+    data = _mnist_loader(batch_size, train=train)
 
-        for X, y in data:
-            yield X, y
+    for X, y in data:
+        yield X, y
