47f63d492e8c9ae5b670bccdb8749342db53624d
Tamera Lanham <tamera.lanham@gmail.com>
2022-04-26 11:55:43 -0700

    feat: gcs util

---
+++ loss_basins/utils/gcs.py
@@ -0,0 +1,46 @@
+import json
+import os
+import shutil
+from google.cloud import storage
+from google.oauth2 import service_account
+from pathlib import Path
+from typing import Union
+
+from numpy import isin
+
+
+class GCS:
+    def __init__(self, bucket_name, key_path=None):
+
+        # Get the service account key
+        if key_path is None:
+            key_path = Path(os.getcwd(), "_keys", os.listdir("_keys")[0])
+        key_path = Path(key_path)
+
+        with open(key_path) as key_file:
+            key = json.load(key_file)
+
+        # Get the bucket
+        storage_credentials = service_account.Credentials.from_service_account_info(key)
+        client = storage.Client(
+            project=key["project_id"], credentials=storage_credentials
+        )
+        self.bucket = client.get_bucket(bucket_name)
+
+    def _upload_dir(self, local_path: Path, bucket_path: str):
+        for path in local_path.iterdir():
+            if path.is_file():
+                self._upload_file(path, bucket_path + "/" + path.name)
+            else:
+                self._upload_dir(path, bucket_path + "/" + path.name)
+
+    def _upload_file(self, local_path: Path, bucket_path: str):
+        gcs_file = self.bucket.blob(bucket_path)
+        gcs_file.upload_from_filename(local_path)
+
+    def upload(self, local_path: Union[str, Path], bucket_path: str):
+        local_path = Path(local_path)
+        if local_path.is_dir():
+            self._upload_dir(local_path, bucket_path)
+        else:
+            self._upload_file(local_path, bucket_path)
---
+++ loss_basins/utils/gcs_test.py
@@ -0,0 +1,60 @@
+from loss_basins.utils.gcs import *
+
+
+def test_init():
+    gcs = GCS("loss-basins")
+    assert isinstance(gcs.bucket, storage.Bucket)
+    blobs = list(gcs.bucket.list_blobs(max_results=3))
+    assert isinstance(blobs[0], storage.Blob)
+
+
+def make_test_files(file_structure, output_dir="./_data/test_files"):
+
+    output_dir = Path(output_dir)
+    if not output_dir.exists():
+        os.makedirs(output_dir)
+
+    def make_files(structure, path):
+        for item in structure:
+            if isinstance(item, str):
+                with open(path / item, "w") as f:
+                    f.write(item + " contents")
+            else:
+                dir_path = path / list(item.keys())[0]
+                os.makedirs(dir_path)
+                make_files(list(item.values())[0], dir_path)
+
+    make_files(file_structure, output_dir)
+
+
+def test_upload():
+    file_structure = [
+        {"dir1": [{"dir3": ["file1", "file2", "file3"]}, "file4", "file5"]},
+        {"dir2": ["file6", "file7"]},
+    ]
+
+    output_dir = "./_data/test_files"
+    make_test_files(file_structure, output_dir)
+
+    gcs = GCS("loss-basins")
+    gcs.upload(output_dir, "test-folder")
+
+    assert (
+        gcs.bucket.get_blob("test-folder/dir1/dir3/file1").download_as_text()
+        == "file1 contents"
+    )
+    assert (
+        gcs.bucket.get_blob("test-folder/dir1/file4").download_as_text()
+        == "file4 contents"
+    )
+    assert (
+        gcs.bucket.get_blob("test-folder/dir2/file7").download_as_text()
+        == "file7 contents"
+    )
+
+    # Clean up GCS
+    blobs = gcs.bucket.list_blobs(prefix="test_files")
+    gcs.bucket.delete_blobs(list(blobs))
+
+    # Clean up local
+    shutil.rmtree(output_dir)
